839a08b93b71d75db76f629b7badaff8bba0fe87
Denys <pedan.denys@gmail.com>
2025-05-16 23:13:40 +1200

    feat: Implement SignUpScreen UI
    This commit introduces the basic UI for the `SignUpScreen` composable.
    
    Key changes:
    - Added `Column` layout to arrange UI elements vertically.
    - Included `Text` elements for title and subtitle.
    - Added `OutlinedTextField` for email, name, and password input.
      - Password field uses `PasswordVisualTransformation`.
    - Added a `Button` for signup action.
    - Implemented `mutableStateOf` for managing the input field values.
    - Styled text elements with `TextStyle`, `FontFamily`, `FontWeight`, and `fontSize`.
    - Applied padding and spacing using `Modifier.padding` and `Spacer`.

--- app/src/main/java/denys/diomaxius/easyshop/screen/SignupScreen.kt
+++ app/src/main/java/denys/diomaxius/easyshop/screen/SignupScreen.kt
@@ -1,9 +1,119 @@
 package denys.diomaxius.easyshop.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun SignUpScreen(modifier: Modifier = Modifier) {
 
+    var email by remember {
+        mutableStateOf("")
+    }
+
+    var name by remember {
+        mutableStateOf("")
+    }
+
+    var password by remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(32.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = "Start your shopping journey now!",
+            style = TextStyle(
+                fontSize = 24.sp,
+                fontFamily = FontFamily.Monospace,
+                fontWeight = FontWeight.Bold
+            )
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = "Create an account",
+            style = TextStyle(
+                fontSize = 20.sp
+            )
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = email,
+            onValueChange = { email = it },
+            label = {
+                Text(text = "Email address")
+            }
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = name,
+            onValueChange = { name = it },
+            label = {
+                Text(text = "Full name")
+            }
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = password,
+            onValueChange = { password = it },
+            label = {
+                Text(text = "password")
+            },
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(60.dp),
+            onClick = {
+
+            }
+        ) {
+            Text(
+                text = "Signup",
+                fontSize = 22.sp
+            )
+        }
+    }
 }
