9f3146ed480f78bd6c06643546af24bb10360c31
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-05-27 21:20:18 +1200

    Refactor: Simplify product retrieval from Firestore
    This commit refactors the `getProducts` method in `ProductRepositoryImpl.kt` to simplify the conversion of Firestore documents to `Product` objects.

--- app/src/main/java/denys/diomaxius/easyshop/data/repository/ProductRepositoryImpl.kt
+++ app/src/main/java/denys/diomaxius/easyshop/data/repository/ProductRepositoryImpl.kt
@@ -1,6 +1,5 @@
 package denys.diomaxius.easyshop.data.repository
 
-import android.util.Log
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.firestore
@@ -13,32 +12,11 @@ class ProductRepositoryImpl(
 ) : ProductRepository {
     override suspend fun getProducts(categoryId: String): List<Product> {
         val doc = firestore
-            .collection("data").
-            document("stock")
+            .collection("data").document("stock")
             .collection("products")
             .whereEqualTo("category", categoryId)
             .get()
             .await()
-        return doc.documents.map {
-            Log.i("Rep Tag", "$it")
-
-            val id = it.getString("id") ?: ""
-            val title = it.getString("title") ?: ""
-            val description = it.getString("description") ?: ""
-            val price = it.getString("price") ?: ""
-            val actualPrice = it.getString("actualPrice") ?: ""
-            val category = it.getString("category") ?: ""
-            val images = it.get("images") as? List<String> ?: emptyList()
-
-            Product(
-                id = id,
-                title = title,
-                description = description,
-                price = price,
-                actualPrice = actualPrice,
-                category = category,
-                images = images
-            )
-        }
+        return doc.documents.mapNotNull { it.toObject(Product::class.java) }
     }
 }
